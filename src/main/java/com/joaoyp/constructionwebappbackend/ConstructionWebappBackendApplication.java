32fc0448d1b99efef86987c88def7ac47f508d43
joaoyp <joaoyp.dev@gmail.com>
2023-11-03 14:36:32 +0000

    Initial Spring Commit

---
+++ src/main/java/com/joaoyp/constructionwebappbackend/ConstructionWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joaoyp.constructionwebappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionWebappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionWebappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoyp/constructionwebappbackend/ConstructionWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoyp.constructionwebappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionWebappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
